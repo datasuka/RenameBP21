02b9617beb25a38808c7fc5f3d8cc3a928486bcc
datasuka <hubungireza@gmail.com>
2025-07-21 22:08:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -102,9 +102,9 @@ st.markdown("3. Anda dapat memilih kolom mana saja sebagai penamaan file.")
 st.markdown("4. Isikan *Custom Awalan Nama File* untuk menentukan awalan nama file, contoh: `Bukti Potong`.")
 st.markdown("5. Klik tombol Rename & Download untuk mengunduh file hasil rename.")
 
-st.markdown("### 📄 Berikut data yang berhasil diekstrak pada tampilan berikut ini:")
+    st.markdown("### 📄 Berikut data yang berhasil diekstrak pada tampilan berikut ini:")
     st.dataframe(df)
-
+    
     selected_cols = st.multiselect("### ✏️ Pilih Kolom untuk Rename", df.columns.tolist())
 
     if st.button("🔁 Rename PDF & Download"):
